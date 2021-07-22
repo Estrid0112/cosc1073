0b53c633d70e25c56d7a6c7ef167d2493caea32b
Estrid He <jiayuan.he@rmit.edu.au>
2021-07-22 15:21:24 +1000

    first commit

---
+++ src/ReadInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ReadInput  {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age = scnr.nextInt();
+        System.out.println("My age is " + age + ".");
+    }
+}
+
